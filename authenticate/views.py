541b40abf7a55095aa49eaab3cebb30b72a5851f
thaikhuong62000 <thaikhuong62000@gmail.com>
2020-07-13 21:56:08 +0700

    Change Login Behavior

--- authenticate/views.py
+++ authenticate/views.py
@@ -11,17 +11,47 @@ def GetLogin(request):
 
 
 def PostLogin(request):
+    inemail = request.POST.get('email')
+    inpassword = request.POST.get('password')
+    myuser = authenticate(username=inemail, password=inpassword)
+    if (myuser is not None):
+        login(request, myuser)
+        if (myuser.user_type == 1):
+            return redirect('customer:view-menu')
+        elif (myuser.user_type == 2):
+            return HttpResponse('Yay, but nothing here')
+        elif (myuser.user_type == 3):
+            return HttpResponse('Yay, but nothing here')
+        elif (myuser.user_type == 4):
+            return redirect('/admin/')
+        else:
+            return HttpResponse('Login failed')
+    else:
+        return HttpResponse('Login failed')
+
+
+def LogOut(request):
+    # Cart.objects.get(customer=request.user.customer).delete()
+    logout(request)
+    return redirect('authenticate:get_login')
+
+
+def GetSignUp(request):
+    return render(request, 'signup/index.html')
+
+
+def PostSignUp(request):
     inusrname = request.POST.get('username')
     inpassword = request.POST.get('password')
     myuser = authenticate(username=inusrname, password=inpassword)
-    if ((myuser is not None) and (myuser.type_account == 'customer')):
+    if ((myuser is not None) and (myuser.user_type == 1)):
         login(request, myuser)
         return redirect('customer:view-menu')
     else:
         return HttpResponse('Login failed')
 
 
-def LogOut(request):
+def SignUp(request):
     Cart.objects.get(customer=request.user.customer).delete()
     logout(request)
     return redirect('authenticate:get_login')
