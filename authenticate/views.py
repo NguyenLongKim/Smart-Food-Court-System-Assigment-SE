a491131926765f5b86c6a406769a274f7629f9a3
thaikhuong62000 <thaikhuong62000@gmail.com>
2020-07-16 08:52:40 +0700

    Fix bug field requirement

--- authenticate/views.py
+++ authenticate/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from model.models.cart import Cart
 from django.http import HttpResponse
-from django.contrib.auth import get_user_model
+from model.models import user
 
 User = get_user_model()
 # Create your views here.
@@ -43,9 +43,30 @@ def GetSignUp(request):
 
 
 def PostSignUp(request):
+    """
+    try:
+        inemail = request.POST.get('email')
+    except ValueError:
+        # if inemail is None:
+        return render(request, 'signup/index.html', {'email_error': 'Email is required'})
+    """
+
     inemail = request.POST.get('email')
+    if inemail == "":
+        return render(request, 'signup/index.html', {'email_error': 'Email is required'})
+
+    qs = User.objects.filter(email=inemail)
+    if qs.exists():
+        return render(request, 'signup/index.html', {'email_error': 'Email is taken'})
+
     inpassword = request.POST.get('password')
+    if inpassword == "":
+        return render(request, 'signup/index.html', {'password_error': 'Password is required'})
+
     inDoB = request.POST.get('date_of_birth')
+    if inDoB == "":
+        return render(request, 'signup/index.html', {'bd_error': 'Birth day is required'})
+
     newuser = User.objects.create_user(
         email=inemail,
         date_of_birth=inDoB,
@@ -56,9 +77,3 @@ def PostSignUp(request):
         return render(request, 'signup/signed.html')
     else:
         return HttpResponse('Sign up failed')
-
-
-def SignUp(request):
-    Cart.objects.get(customer=request.user.customer).delete()
-    logout(request)
-    return redirect('authenticate:get_login')
