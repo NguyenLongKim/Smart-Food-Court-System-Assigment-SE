268240f3aa3aee526f90800186c234409e2dc53d
Quyromua1234 <quyromua@gmail.com>
2020-07-30 16:51:25 +0700

    update views.py, remove HttpResponse

--- vendorowner/views.py
+++ vendorowner/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect
 from .forms import AddNewFoodForm
 from .vendorowner import VendorOwnerServices
 from model.models.food import Food 
